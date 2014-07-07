9f88b8fa74508295ac812d9abd39ba98a96124ec
arkmal <damsarkmal@gmail.com>
2014-07-07 19:25:02 +0200

    Create Neutralgems.hpp

---
+++ script/CfgServerTrader/Neutralgems.hpp
@@ -0,0 +1,42 @@
+class Category_700 {
+	class ItemRuby {
+		type = "trade_items";
+		buy[] = {12,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class ItemTopaz {
+		type = "trade_items";
+		buy[] = {12,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class ItemObsidian {
+		type = "trade_items";
+		buy[] = {12,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class ItemSapphire {
+		type = "trade_items";
+		buy[] = {12,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class ItemAmethyst {
+		type = "trade_items";
+		buy[] = {12,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class ItemEmerald {
+		type = "trade_items";
+		buy[] = {12,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class ItemCitrine {
+		type = "trade_items";
+		buy[] = {12,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+class Category_701 {
+	class ItemKiloHemp {
+		type = "trade_items";
+		buy[] = {5,"ItemGoldBar10oz"};
+		sell[] = {5,"ItemGoldBar"};
+};
