fae2e8851248c3647308a0f455edf97494da7c02
arkmal <damsarkmal@gmail.com>
2014-07-07 19:01:33 +0200

    Create NeutralHelicopterUnarmed.hpp

---
+++ script/CfgServerTrader/NeutralHelicopterUnarmed.hpp
@@ -0,0 +1,72 @@
+class Category_519 {
+	class Mi17_Civilian_DZ {
+		type = "trade_any_vehicle";
+		buy[] = {5,"ItemBriefcase100oz"};
+		sell[] = {8,"ItemGoldBar10oz"};
+	};
+	class AH6X_DZ {
+		type = "trade_any_vehicle";
+		buy[] = {2,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class MH6J_DZ {
+		type = "trade_any_vehicle";
+		buy[] = {2,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class CSJ_GyroC {
+		type = "trade_any_vehicle";
+		buy[] = {1,"ItemGoldBar10oz"};
+		sell[] = {5,"ItemGoldBar"};
+	};
+	class CSJ_GyroCover {
+		type = "trade_any_vehicle";
+		buy[] = {1,"ItemGoldBar10oz"};
+		sell[] = {5,"ItemGoldBar"};
+	};
+	class CSJ_GyroP {
+		type = "trade_any_vehicle";
+		buy[] = {1,"ItemGoldBar10oz"};
+		sell[] = {5,"ItemGoldBar"};
+	};
+	class BAF_Merlin_DZE {
+		type = "trade_any_vehicle";
+		buy[] = {10,"ItemBriefcase100oz"};
+		sell[] = {1,"ItemBriefcase100oz"};
+	};
+	class pook_H13_civ_ru {
+		type = "trade_any_vehicle";
+		buy[] = {3,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class pook_H13_civ_ru_white {
+		type = "trade_any_vehicle";
+		buy[] = {3,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class pook_H13_civ_ru_slate {
+		type = "trade_any_vehicle";
+		buy[] = {3,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class pook_H13_civ_ru_black {
+		type = "trade_any_vehicle";
+		buy[] = {3,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class pook_H13_civ_ru_yellow {
+		type = "trade_any_vehicle";
+		buy[] = {3,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class pook_H13_medevac {
+		type = "trade_any_vehicle";
+		buy[] = {3,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+	class pook_H13_medevac_CIV_RU {
+		type = "trade_any_vehicle";
+		buy[] = {3,"ItemBriefcase100oz"};
+		sell[] = {5,"ItemGoldBar10oz"};
+	};
+};
