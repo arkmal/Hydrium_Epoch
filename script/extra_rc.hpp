89d55ca7fa9717c634d025d3b8c1321696adfee5
arkmal <damsarkmal@gmail.com>
2014-07-07 19:26:58 +0200

    Create extra_rc.hpp

---
+++ script/extra_rc.hpp
@@ -0,0 +1,56 @@
+class ExtraRc {
+	class glock17_EP1 {
+        class CommitSuicide {
+            text = "Se suicider";
+            script = "execVM ""script\suicide.sqf"";";
+        };
+    };
+    class M9 {
+        class CommitSuicide {
+            text = "Se suicider";
+            script = "execVM ""script\suicide.sqf"";";
+        };
+    };  
+    class M9SD {
+        class CommitSuicide {
+            text = "Se suicider";
+            script = "execVM ""script\suicide.sqf"";";
+        };
+    };  
+    class Makarov {
+        class CommitSuicide {
+            text = "Se suicider";
+            script = "execVM ""script\suicide.sqf"";";
+        };
+    };
+    class revolver_EP1 {
+        class CommitSuicide {
+            text = "Se suicider";
+            script = "execVM ""script\suicide.sqf"";";
+        };
+    };  
+    class Colt1911 {
+        class CommitSuicide {
+            text = "Se suicider";
+            script = "execVM ""script\suicide.sqf"";";
+        };
+    };      
+    class revolver_gold_EP1 {
+        class CommitSuicide {
+            text = "Se suicider";
+            script = "execVM ""script\suicide.sqf"";";
+        };
+    };  
+    class MakarovSD {
+        class CommitSuicide {
+            text = "Se suicider";
+            script = "execVM ""script\suicide.sqf"";";
+        };
+    };
+	class ItemKiloHemp {
+        class smokeweed {
+            text = "Smoke the shit";
+            script = "execVM 'script\smokeshit.sqf'";
+        };
+	};
+};
